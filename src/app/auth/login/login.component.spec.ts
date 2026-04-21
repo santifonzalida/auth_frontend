778b4661df93535a218a1fbeed42b7434845628c
Santiago <santiifonzalida@gmail.com>
2026-04-21 00:02:24 -0300

    pruebas unitarias

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,22 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { of, throwError } from 'rxjs';
+import { vi } from 'vitest';
 
-import { Login } from './login';
-
-describe('Login', () => {
-  let component: Login;
-  let fixture: ComponentFixture<Login>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let authServiceMock: any;
+  let routerMock: any;
 
   beforeEach(async () => {
+    authServiceMock = {
+      login: vi.fn()
+    };
+
+    routerMock = {
+      navigate: vi.fn()
+    };
+
     await TestBed.configureTestingModule({
-      imports: [Login],
+      imports: [ReactiveFormsModule, LoginComponent],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: Router, useValue: routerMock }
+      ]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Login);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('crea el componente', () => {
     expect(component).toBeTruthy();
   });
+
+  it('form inválido si está vacío', () => {
+    expect(component.loginForm.valid).toBe(false);
+  });
+
+  it('login exitoso navega al dashboard', () => {
+    component.loginForm.setValue({
+      username: 'admin',
+      password: '1234'
+    });
+  });
+
 });
